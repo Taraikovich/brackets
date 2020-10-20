60a8b6c3f80ccde6179b73cb03e51215059b4a51
Taraikovich <taraikovi4@ya.ru>
2020-10-20 19:06:22 +0300

    init: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split('');
+  let bracketsPair = [];
+    
+  for (i = 0; str.length > 0; ) {
+    if (str[i + 1] === undefined) {
+      return false;
+    } else if (bracketsConfig.join('').includes([str[i], str[i + 1]])) {
+      bracketsPair.push([str[i], str[i + 1]]);
+      str.splice(i + 1, 1);
+      str.splice(i, 1);
+      i = 0;
+    } else {
+      i += 1;
+    }
+  }
+ 
+  for (i = 0; i < bracketsPair.length; i++) {
+    if (bracketsConfig.join('').includes(bracketsPair[i]) === false) {
+      return false;
+    }
+  }
+  return true;
 }
